1486fd23986b9417a35781cdc5e0bd1dd7b1a8d7
Carter Harrington <charring@unb.ca>
2020-02-15 17:16:51 -0400

    Pages Added: Users, Cars, PickUpPoints

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 //You will need to import your page here.
 import CarList from './cars/CarList';
 import Home from './home/home';
+import UserList from './users/UserList';
+import PickUpPointList from './pickUpPoints/PickUpPointList'
 
 const Main = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home}></Route>
       <Route exact path='/cars' component={CarList}></Route>
+      <Route exact path='/users' component={UserList}></Route>
+      <Route exact path='/pick-up-points' component={PickUpPointList}></Route>
     </Switch>
   );
 }
---
+++ src/components/pickUpPoints/PickUpPointList.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import Grid from '@material-ui/core/Grid';
+import PickUpPoint from './PickUpPoint'
+import PickUpPointService from  '../../apiServices/services/pickUpPoint-service';
+
+class PickUpPointList extends Component {
+  state = {
+    pickUpPoints: []
+  }
+
+  constructor() {
+    super()
+    this.pickUpPointService = new PickUpPointService(process.env.REACT_APP_API_KEY);
+    this.getPickUpPoints();
+  }
+
+  getPickUpPoints = () => {
+    this.pickUpPointService.getAll()
+    .then((response) => {
+      this.setState({pickUpPoints: response})
+      console.log("\nResponse:\n")
+      console.dir(response);
+    })
+    .catch((error) => {
+      console.log("Error occured while gettting all cars.")
+      console.log(error);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.pickUpPoints ? (
+          <div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <Grid container spacing={2} style={{padding: 12}}>
+              { this.state.pickUpPoints.map(currentPickUpPoint => (
+                <Grid key={currentPickUpPoint.id} item xs={12}>
+                  <PickUpPoint pickUpPoint={currentPickUpPoint} />
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+        ) : "No pick up points found."}
+      </div>
+
+    )
+  }
+}
+
+export default PickUpPointList;
